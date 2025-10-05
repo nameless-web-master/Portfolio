ed2c05e24cc1c619bbae263526d6a2c2a30a553d
nameless <nameless20001027@gmail.com>
2025-10-05 16:10:41 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ import { bwmap, worldmap } from '../assets';
 const Hero = () => {
   return (
     <>
-      <div className="absolute top-0 left-0 z-0 h-[100vh]">
+      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
         <img
           src={bwmap}
           alt="world map"
           className="w-full h-full sm:block hidden object-cover"
         />
       </div>
-      <div className="absolute top-0 left-0 z-0 h-[100vh]">
+      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
         <img
           src={worldmap}
           alt="world map"
